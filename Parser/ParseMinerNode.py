fc4ea7922acd77afc372a90009d8200d43413cb1
atsta <atstamatiou@gmail.com>
2022-06-01 22:38:58 +0300

    parsers1

--- Parser/ParseMinerNode.py
+++ Parser/ParseMinerNode.py
@@ -1,18 +1,14 @@
 import gzip
 import csv
 
-blocks_csv = csv.writer(open('../Data/Blocks.csv', 'w'))
-
-#print("blockId:ID,hash,time,:LABEL")
-header = ['blockId:ID','hash','time',':LABEL']
+blocks_csv = csv.writer(open('../Data/Miners.csv', 'w'))
+header = ['guessed_miner_id:ID',':LABEL']
 blocks_csv.writerow(header)
-
 for i in range(10,31): 
     print(i)
     with gzip.open('../Data/Blocks/blockchair_bitcoin_blocks_202104' + str(i) + '.tsv.gz', "rt") as f:
         f.readline()
         for line in f:
             fields = line.strip().split('\t')
-            row = [fields[0], fields[1], fields[2], "Block"]
-            #print("%s,%s,%s,Block" % (fields[0], fields[1], fields[2]))
+            row = [fields[-1], "Guessed_miner"]
             blocks_csv.writerow(row)
--- Parser/ParseRewardedRelationship.py
+++ Parser/ParseRewardedRelationship.py
@@ -1,18 +1,15 @@
 import gzip
 import csv
 
-blocks_csv = csv.writer(open('../Data/Blocks.csv', 'w'))
-
-#print("blockId:ID,hash,time,:LABEL")
-header = ['blockId:ID','hash','time',':LABEL']
+blocks_csv = csv.writer(open('../Data/Rewards.csv', 'w'))
+header = [':START_ID','reward','reward_usd',':END_ID',':TYPE']
 blocks_csv.writerow(header)
-
 for i in range(10,31): 
     print(i)
     with gzip.open('../Data/Blocks/blockchair_bitcoin_blocks_202104' + str(i) + '.tsv.gz', "rt") as f:
         f.readline()
         for line in f:
             fields = line.strip().split('\t')
-            row = [fields[0], fields[1], fields[2], "Block"]
-            #print("%s,%s,%s,Block" % (fields[0], fields[1], fields[2]))
+            #blockId, reward, reward_usd, guessed_miner
+            row = [fields[0], fields[-3], fields[-2], fields[-1], "REWARDED"]
             blocks_csv.writerow(row)
